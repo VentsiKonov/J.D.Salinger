9563276528a006863896193d079f0641c2b7789f
vaso92 <vasko-vasilev@hotmail.com>
2014-02-20 20:31:32 +0200

    MainCharacter.cs
    Our main character.

---
+++ Waits/TheGame/MainCharacter.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waits
+{
+    class MainCharacter : Character
+    {
+        public MainCharacter(string name, Sex sex)
+            : base(name, sex) { }
+
+    }
+}
