84173ab704b28f952ae48b445b0895528bdb3b83
Tancho Mihov <tancho.mihov@gmail.com>
2014-02-24 19:37:27 +0200

    Play songs by number

--- Waits/TheGame/PlayerMP3.cs
+++ Waits/TheGame/PlayerMP3.cs
@@ -3,14 +3,18 @@ using NAudio.Wave;
 
 namespace Waits
 {
+
     public static class PlayerMP3
     {
 
-        public static void Play(string fileName)
+        private const string path = @"../../SongStorage/song";
+        private const string ext = @".mp3";
+        public static void Play(int songNumber)
         {
+
             using (DirectSoundOut output = new DirectSoundOut())
             {
-                using (Mp3FileReader reader = new Mp3FileReader(fileName))
+                using (Mp3FileReader reader = new Mp3FileReader(path + songNumber + ext))
                 {
                     output.Init(reader);
                     output.Play();
