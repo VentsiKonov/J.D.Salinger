6e4f3a417ef37d15b19b92ef7b742de2167ec8d2
ADimanova <addi_91@abv.bg>
2014-02-24 16:04:35 +0200

    Engine

---
+++ Waits/TheGame/GameEngine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Waits
+{
+    public class GameEngine
+    {
+        private IRenderer renderer;
+        private int waitMsPerTick;
+        public IList<IRenderable> itemsToRender;
+         public GameEngine(IRenderer inputRenderer, List<IRenderable> items = null, int waitMs = 1000)
+        {
+            this.renderer = inputRenderer;
+            this.waitMsPerTick = waitMs;
+
+            if (items != null)
+            {
+                this.itemsToRender = items;
+            }
+            else
+            {
+                this.itemsToRender = new List<IRenderable>();
+            }
+        }
+
+         public void Run()
+         {
+             while (true)
+             {
+                 renderer.RenderAll();
+
+                 renderer.ClearQueue();
+
+                 foreach (var item in this.itemsToRender)
+                 {
+                     renderer.EnqueueForRendering(item);
+                 }
+
+                 Thread.Sleep(this.waitMsPerTick);
+             }
+         }
+    }
+}
