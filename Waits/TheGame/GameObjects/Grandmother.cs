16dc192cee60963a3ef451adde1ecdf99c12bc03
Ventsi Konov <ventsi.konov@gmail.com>
2014-02-25 21:24:18 +0200

    Uncomment Grandmother class

--- Waits/TheGame/GameObjects/Grandmother.cs
+++ Waits/TheGame/GameObjects/Grandmother.cs
@@ -1,17 +1,17 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Waits
-//{
-//    public class Grandmother : Character
-//    {
+namespace Waits
+{
+    public class Grandmother : Character
+    {
 
-//        public override MatrixCoords Move(MatrixCoords changeWith)
-//        {
-//            throw new NotImplementedException();
-//        }
-//    }
-//}
+        public override MatrixCoords Move(MatrixCoords changeWith)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
