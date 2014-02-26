a13c03226bcaf33973b3b6a463d7d3b5c388dbea
Tancho Mihov <tancho.mihov@gmail.com>
2014-02-26 21:59:30 +0200

    RandomGameObjects Creator

--- Waits/TheGame/TheGame.cs
+++ Waits/TheGame/TheGame.cs
@@ -5,25 +5,30 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
-    
+
     class TheGame
     {
         static void Main()
         {
+
+
             MainCharacter hero = new MainCharacter("User", Sex.Male, new MatrixCoords(0, 3), new List<ISellable>(),
                 new List<Song>());
 
-            Grandmother hall = new Grandmother("baba", Sex.Female, new MatrixCoords(4, 4));
+            //Grandmother hall = new Grandmother("baba", Sex.Female, new MatrixCoords(4, 4));
 
-            MarketPlace market = new MarketPlace("market", new MatrixCoords(7, 7));
+            //MarketPlace market = new MarketPlace("market", new MatrixCoords(7, 7));
 
-            House house = new House("nane", new MatrixCoords(3,7), 2);
+            //House house = new House("nane", new MatrixCoords(3,7), 2);
+
+
 
             GridDrawer.Init();
-
+            GameObjectCreator listOfObjects = new GameObjectCreator(8);
             GridDrawer.StartScreen();
 
             GridDrawer.Init();
+            
 
             while (true)
             {
