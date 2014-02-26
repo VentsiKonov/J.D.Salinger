132cf904c54b1344a4e467b2048f8ada0b9afd3e
vaso92 <vasko-vasilev@hotmail.com>
2014-02-26 17:53:08 +0200

    Added GetSong and WantsSomething() methods.

--- Waits/TheGame/GameObjects/Grandmother.cs
+++ Waits/TheGame/GameObjects/Grandmother.cs
@@ -8,16 +8,34 @@ namespace Waits
 {
     public class Grandmother : Character
     {
+        public const int NumberOfItems = 2;
 
         public override void Move(MatrixCoords changeWith)
         {
             throw new NotImplementedException();
         }
 
-        public Grandmother(string name, Sex sex, MatrixCoords position)
+        public Grandmother(string name, Sex sex, MatrixCoords position) 
             : base(name, sex, position)
         {
+            Random randomSong = new Random();
+            randomSong.Next(0, Enum.GetNames(typeof(Song)).Length);
+        }
 
+        public Song GetSong
+        {
+            get 
+            {
+                Random randomSong = new Random();
+                return (Song)randomSong.Next(0, Enum.GetNames(typeof(Song)).Length);
+            }
+            private set;
+        }
+
+        public int WantsSomething()
+        {
+            Random randomSomething = new Random();
+            return randomSomething.Next(0, NumberOfItems + 1);
         }
     }
 }
